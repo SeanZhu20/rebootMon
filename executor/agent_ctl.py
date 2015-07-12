41df8599d09d1c89dd51fa340b577aa66cd1f6ea
auxten <auxtenwpc@gmail.com>
2015-07-12 11:44:45 +0800

    agent_ctl tobe finished

--- executor/agent_ctl.py
+++ executor/agent_ctl.py
@@ -4,6 +4,8 @@ from crypt import *
 
 from multiprocessing import Pool
 
+# agent_ctl.py 模块名 部署路径 机器列表（逗号分隔）
+# 发送json到机器列表里的agent {"pkg_name":"package_name", "path":"deploy_path"}
 def rpc_call(host):
     c = zerorpc.Client()
     c.connect("tcp://%s"%(host))
